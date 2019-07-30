165050f0297a26a63a3db64a9207b0b8e442f42c
Самойлов Олег Дмитриевич <odsamoylov@domclick.ru>
2019-07-30 17:40:10 +0300

    Тюнинг psql (--nopsqlrc)

--- heartbeat/reader1.sh
+++ heartbeat/reader1.sh
@@ -1,1 +1,4 @@
-LC_NUMERIC=en_US.UTF-8 watch -n 0.1 --exec psql --dbname="postgresql://heartbeat:ChangeMe@krogan1s1/heartbeat?connect_timeout=2&application_name=reader1.sh&keepalives=1&keepalives_idle=1&keepalives_interval=1&keepalives_count=1&target_session_attrs=any" --command='select beat from heartbeat'
+LC_NUMERIC=en_US.UTF-8 watch -n 0.1 --exec psql \
+	--dbname="postgresql://heartbeat:ChangeMe@krogan1s1/heartbeat?connect_timeout=2&application_name=reader1.sh&keepalives=1&keepalives_idle=1&keepalives_interval=1&keepalives_count=1&target_session_attrs=any" \
+	--command='select beat from heartbeat' \
+	--no-align --quiet --tuples-only --no-psqlrc
