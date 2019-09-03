16632da8c30331c555d0684f1f2611cc5367986e
Самойлов Олег Дмитриевич <odsamoylov@domclick.ru>
2019-09-03 17:34:14 +0300

    В hearbeat вместо скриптовых файлов в выполняемые

--- heartbeat/heart1.sh
+++
@@ -1,4 +0,0 @@
-LC_NUMERIC=en_US.UTF-8 watch -n 0.1 --exec psql \
-	--dbname="postgresql://heartbeat:ChangeMe@krogan1/heartbeat?connect_timeout=2&application_name=heart1.sh&keepalives=1&keepalives_idle=1&keepalives_interval=1&keepalives_count=1&target_session_attrs=read-write" \
-	--command='update heartbeat set beat=now() returning beat' \
-	--no-align --quiet --tuples-only --no-psqlrc
