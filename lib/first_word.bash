3300a6650f0fcae2a28c52b5d6b775f696dc8ade
Самойлов Олег Дмитриевич <odsamoylov@domclick.ru>
2019-09-12 13:37:46 +0300

    lib/first_word.bash
    Возвращает первое слово из списка.

---
+++ lib/first_word.bash
@@ -0,0 +1,10 @@
+# $@ список слов, возвращает первое из них
+# Должно существовать хотя бы одно слово
+. "${lib_dir}/is_function_absent.bash"
+if is_function_absent 'first_word'
+then
+	function first_word {
+		echo "$1"
+	}
+	readonly -f first_word
+fi
